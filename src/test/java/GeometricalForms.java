4d5f110318b1c2362e354e8202cc7c5eb3165be7
DanlevPurple <daniel.levin@purplescout.se>
2022-06-07 17:07:23 +0200

    Start
    Junit testing works

---
+++ src/test/java/GeometricalForms.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GeometricalForms {
+
+    @Test
+    void should_returnLine_when_TwoPointsAreGive(){
+
+        assertEquals("5", "5");
+    }
+
+
+
+    }
+
